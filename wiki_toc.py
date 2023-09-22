6104c801221296ceec113f080ccbe66fb89f3157
SilentJMA <kilwanitro@gmail.com>
2023-09-22 12:21:31 +0200

    Added error handling, combined the url and file name pairs, used a list comprehension to simplify the creation, removed the duplicate import of the requests module, adjust readibality

--- wiki_toc.py
+++ wiki_toc.py
@@ -1,24 +1,18 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
-import requests
-
 
 def get_data(url):
     response = requests.get(url)
+    response.raise_for_status()  # Add error handling for request
     soup = BeautifulSoup(response.text, 'lxml')
-    table_of_contents = soup.find("div", id="toc")
-    headings = table_of_contents.find_all("li")
-    data = []
-    for heading in headings:
-        heading_text = heading.find("span", class_="toctext").text
-        heading_number = heading.find("span", class_="tocnumber").text
-        data.append({
-            'heading_number': heading_number,
-            'heading_text': heading_text,
-        })
+    headings = soup.find("div", id="toc").find_all("li")
+    
+    data = [{'heading_number': heading.find("span", class_="tocnumber").text,
+             'heading_text': heading.find("span", class_="toctext").text}
+            for heading in headings]
+    
     return data
-
 
 def export_data(data, file_name):
     with open(file_name, "w", newline="") as file:
@@ -26,20 +20,17 @@ def export_data(data, file_name):
         writer.writeheader()
         writer.writerows(data)
 
-
 def main():
-    url_to_parse = "https://en.wikipedia.org/wiki/Python_(programming_language)"
-    file_name = "python_toc.csv"
-    data = get_data(url_to_parse)
-    export_data(data, file_name)
-
-    url_to_parse = "https://en.wikipedia.org/wiki/Web_scraping"
-    file_name = "web_scraping_toc.csv"
-    data = get_data(url_to_parse)
-    export_data(data, file_name)
+    urls = [
+        ("https://en.wikipedia.org/wiki/Python_(programming_language)", "python_toc.csv"),
+        ("https://en.wikipedia.org/wiki/Web_scraping", "web_scraping_toc.csv")
+    ]
+    
+    for url, file_name in urls:
+        data = get_data(url)
+        export_data(data, file_name)
 
     print('Done')
-
 
 if __name__ == '__main__':
     main()
